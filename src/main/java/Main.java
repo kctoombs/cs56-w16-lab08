4dedb3c622fce7b9130fa932ee56157d3f345e31
Kenneth Toombs <kctoombs@umail.ucsb.edu>
2016-02-25 16:33:04 -0800

    database step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,10 +25,14 @@ public class Main {
     staticFileLocation("/public");
 
     get("/hello", (req, res) -> {
-	    RelativisticModel.select();
-	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-	    return "E=mc^2: 12 GeV = " + m.toString();
-	});
+      RelativisticModel.select();
+
+      String energy = System.getenv().get("ENERGY");
+
+      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+      return "E=mc^2: " + energy + " = " + m.toString();
+    });
+    
     get("/ucsb", (req, res) -> "Go Gauchos");
 
     get("/", (request, response) -> {
@@ -37,6 +41,7 @@ public class Main {
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
+
 
     get("/db", (req, res) -> {
       Connection connection = null;
@@ -53,6 +58,33 @@ public class Main {
         while (rs.next()) {
           output.add( "Read from DB: " + rs.getTimestamp("tick"));
         }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+    }, new FreeMarkerEngine());
+
+    
+    /*get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+	  }*/
 
         attributes.put("results", output);
         return new ModelAndView(attributes, "db.ftl");
